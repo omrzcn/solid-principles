39c5b0044c027a9f3fc54c9d40761c55ef4ed812
omrzcn <omrzcnn@gmail.com>
2024-03-09 12:15:36 +0300

    good types

---
+++ solid/src/main/java/com/cydeo/SolidPrinciples/singleResponsibility/example/good/Manager.java
@@ -0,0 +1,15 @@
+package com.cydeo.SolidPrinciples.singleResponsibility.example.good;
+
+public class Manager {
+
+    public static boolean validate() {
+        // Check validation
+        return true;
+    }
+
+    public static boolean checkSecurity() {
+        // Send to security related part
+        return true;
+    }
+
+}
---
+++ solid/src/main/java/com/cydeo/SolidPrinciples/singleResponsibility/example/good/UserController.java
@@ -0,0 +1,21 @@
+package com.cydeo.SolidPrinciples.singleResponsibility.example.good;
+
+public class UserController {
+    // this class is only responsible for these methods.
+
+    public static void getRequest() {
+        // Get the request
+        // Send this request to validation and security part
+    }
+
+    public static String createResponse() {
+        return "Response";  // For example "User is created"
+    }
+
+    public static void sendResponse() {
+        // Send user a response
+    }
+
+}
+
+
